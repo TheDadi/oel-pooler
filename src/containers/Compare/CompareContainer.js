663e559529f9947499acd3fb14067c6d310d74a8
dariodifazio <d>
2017-05-12 13:26:40 +0200

    work fin

--- src/containers/Compare/CompareContainer.js
+++ src/containers/Compare/CompareContainer.js
@@ -3,12 +3,18 @@ import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import * as CompareActionCreators from "./actions";
 import Box from "grommet/components/Box";
+import Button from "grommet/components/Button";
 import Heading from "grommet/components/Heading";
+import List from "grommet/components/List";
+import ListItem from "grommet/components/ListItem";
+import Table from "grommet/components/Table";
+import TableHeader from "grommet/components/TableHeader";
+import TableRow from "grommet/components/TableRow";
 import Paragraph from "grommet/components/Paragraph";
 import Timestamp from "grommet/components/Timestamp";
 
 class LandingContainer extends Component {
-  render () {
+  render() {
     const {
       actions:{
         inputChanged,
@@ -34,20 +40,52 @@ class LandingContainer extends Component {
     } = this.props
     return (
       <div>
-        <Box align="center" pad={{ vertical: "large", between: "small" }}>
-          <Box pad={{ horizontal: "large" }} align="center" textAlign="center"
-               size={{ "width": { "max": "xxlarge" } }}>
+        <Box align="center" pad={{vertical: "large", between: "small", horizontal: 'large'}}>
+          <Box pad={{horizontal: "large"}} align="start" textAlign="start"
+               size={{"width": {"max": "xxlarge"}}}>
+            <Box pad={{vertical: "large"}}>
             <Heading tag="h1" strong={true}>
               Ihre Angebote
             </Heading>
             <Paragraph size="medium" width="large" margin="none">
               {'Datum: '}
-              <Timestamp value={firstDate} fields='date' />
+              <Timestamp value={firstDate} fields='date'/>
               {' - '}
-              <Timestamp value={lastDate} fields='date' />
+              <Timestamp value={lastDate} fields='date'/>
             </Paragraph>
+            </Box>
+            <Table responsive={true}>
+              <TableHeader  labels={['Anbieter',	'Preis* / 100l',	'Total*']} />
+              <TableRow>
+                <td>
+                  Oel Pool
+                </td>
+                <td>
+                  CHF 75.70
+                </td>
+                <td>
+                  CHF 4'540.75
+                </td>
+                <td>
+                  <Button primary={true} onClick={()=> console.log('button clicked')}>Bestellen</Button>
+                </td>
+              </TableRow>
+              <TableRow>
+                <td>
+                  Kuhn
+                </td>
+                <td>
+                  CHF 80.00
+                </td>
+                <td>
+                  CHF 5'540.75
+                </td>
+                <td>
+                  <Button primary={true} onClick={()=> console.log('button clicked')}>Bestellen</Button>
+                </td>
+              </TableRow>
+            </Table>
           </Box>
-
         </Box>
       </div>
     )

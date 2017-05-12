84f0e2225ea4a318011b29c91e67aa44f92afced
dariodifazio <d>
2017-05-12 17:11:43 +0200

    blubb

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const compress = require('compression')
+const path = require('path')
+const fs = require('fs')
 console.log('test')
 const app = express()
 
@@ -12,9 +14,17 @@ app.use(express.static('./dist'))
 // Define the port to run on
 app.set('port', process.env.PORT);
 
-app.get('*', function(req, res){
-  res.sendFile('./dist/index.html');
-});
+app.use('*', function (req, res, next) {
+  const filename = path.join('./dist', 'index.html')
+  fs.readFile(filename, (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.set('content-type', 'text/html')
+    res.send(result)
+    res.end()
+  })
+})
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;

ec4a0bfd3186e2489d41dce95bf39d3e76418171
dariodifazio <d>
2017-05-12 16:52:48 +0200

    dep

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(express.static('./dist'))
 // Define the port to run on
 app.set('port', process.env.PORT);
 
+app.get('*', function(req, res){
+  res.sendFile('index.html');
+});
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
